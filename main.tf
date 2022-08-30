6e976d812c5a02d54fbbd78c3abac14389fd2f24
anandkasina <106368078+anandkasina@users.noreply.github.com>
2022-08-30 12:34:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@ module "vpcc_terraform-google-vpc" {
   source  = "app.terraform.io/DEMO111/vpcc/google//modules/terraform-google-vpc"
   version = "1.1.2"
   # insert required variables here
+  region = var.region
+  name = var.name
 }
